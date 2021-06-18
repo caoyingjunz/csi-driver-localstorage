4807ea4c84666bcb95bc3ed27e4953734269ec67
chenggangyang <1451571361@qq.com>
2021-06-19 00:34:32 +0800

    Update helper.go

--- cmd/pixiu-controller-manager/app/helper.go
+++ cmd/pixiu-controller-manager/app/helper.go
@@ -1,12 +1,9 @@
 /*
 Copyright 2021.
-
 Licensed under the Apache License, Version 2.0 (the "License");
 you may not use this file except in compliance with the License.
 You may obtain a copy of the License at
-
     http://www.apache.org/licenses/LICENSE-2.0
-
 Unless required by applicable law or agreed to in writing, software
 distributed under the License is distributed on an "AS IS" BASIS,
 WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -47,15 +44,6 @@ func WaitForAPIServer(client clientset.Interface, timeout time.Duration) error {
 
 		return true, nil
 	})
-
-func StartHealthzServer(healthzHost string, healthzPort string) {
-	http.HandleFunc("/healthz", func(w http.ResponseWriter, _ *http.Request) {
-		w.WriteHeader(200)
-		w.Write([]byte("ok"))
-	})
-
-	klog.Infof("Starting Healthz Server...")
-	klog.Fatal(http.ListenAndServe(healthzHost+":"+healthzPort, nil))
 
 	if err != nil {
 		return fmt.Errorf("%v: %v", err, lastErr)
