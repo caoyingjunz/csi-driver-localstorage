d3a8b4e506eccb5ed983fd21dfcd4faac5296508
caoyingjun <cao.yingjunz@gmail.com>
2021-06-26 15:18:40 +0800

    Add pixiu webhook supported

--- config/webhook/deploy.sh
+++ config/webhook/deploy.sh
@@ -24,7 +24,7 @@ keydir="$(mktemp -d)"
 echo "Generating TLS keys ..."
 "generate-keys.sh" "$keydir"
 
-# Create the `webhook-demo` namespace. This cannot be part of the YAML file as we first need to create the TLS secret,
+# Create the `pixiu-system` namespace. This cannot be part of the YAML file as we first need to create the TLS secret,
 # which would fail otherwise.
 echo "Creating Kubernetes objects ..."
 kubectl create namespace pixiu-system
@@ -37,7 +37,7 @@ kubectl -n pixiu-system create secret tls webhook-server-tls \
 # Read the PEM-encoded CA certificate, base64 encode it, and replace the `${CA_PEM_B64}` placeholder in the YAML
 # template with it. Then, create the Kubernetes resources.
 ca_pem_b64="$(openssl base64 -A <"${keydir}/ca.crt")"
-sed -e 's@${CA_PEM_B64}@'"$ca_pem_b64"'@g' <"admission-controller-deployment.yaml" \
+sed -e 's@${CA_PEM_B64}@'"$ca_pem_b64"'@g' <"pixiu-webhook-controller.yaml" \
     | kubectl create -f -
 
 # Delete the key directory to prevent abuse (DO NOT USE THESE KEYS ANYWHERE ELSE).
