35fcd4e6ee0ca66c13b7a15dea8a53072fb60088
caoyingjunz <cao.yingjunz@gmail.com>
2021-07-19 00:04:13 +0800

    [part1]Add pixruntime as interface for image prepared #85

---
+++ pkg/controller/pixruntime/pixruntime.go
@@ -0,0 +1,38 @@
+/*
+Copyright 2021 The Pixiu Authors.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package pixruntime
+
+import (
+	"os"
+)
+
+const (
+	dockerSocket = "/var/run/docker.sock"
+	dockerHost   = "unix://" + dockerSocket
+
+	containerdSocket = "/run/containerd/containerd.sock"
+)
+
+// isExistingSocket checks if path exists and is domain socket
+func isExistingSocket(path string) bool {
+	fileInfo, err := os.Stat(path)
+	if err != nil {
+		return false
+	}
+
+	return fileInfo.Mode()&os.ModeSocket != 0
+}
