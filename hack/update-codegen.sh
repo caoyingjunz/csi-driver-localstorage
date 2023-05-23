dbfef65e02b8a5dd75fa66c9753098ce19c8d5c4
caoyingjunz <cao.yingjunz@gmail.com>
2023-05-24 00:15:00 +0800

    add

--- hack/update-codegen.sh
+++ hack/update-codegen.sh
@@ -19,23 +19,15 @@ set -o nounset
 set -o pipefail
 
 SCRIPT_ROOT=$(dirname "${BASH_SOURCE[0]}")/..
-CODEGEN_PKG=${CODEGEN_PKG:-$(cd "${SCRIPT_ROOT}"; ls -d -1 ./vendor/k8s.io/code-generator 2>/dev/null || echo ../code-generator)}
 
-source "${CODEGEN_PKG}/kube_codegen.sh"
+echo $SCRIPT_ROOT
+./../code-generator/generate-groups.sh all \
+github.com/caoyingjunz/csi-driver-localstorage/pkg/client \
+github.com/caoyingjunz/csi-driver-localstorage/pkg/apis \
+pixiu.caoyingjunz.io:v1 \
+# 指定头文件
+--go-header-file=/Users/caoyuan/workstation/csi-driver-localstorage/hack/boilerplate.go.txt \
+# 指定输出位置，默认为GOPATH
+--output-base ./
 
-# generate the code with:
-# --output-base    because this script should also be able to run inside the vendor dir of
-#                  k8s.io/kubernetes. The output-base is needed for the generators to output into the vendor dir
-#                  instead of the $GOPATH directly. For normal projects this can be dropped.
-
-kube::codegen::gen_helpers \
-    --input-pkg-root k8s.io/sample-controller/pkg/apis \
-    --output-base "$(dirname "${BASH_SOURCE[0]}")/../../.." \
-    --boilerplate "${SCRIPT_ROOT}/hack/boilerplate.go.txt"
-
-kube::codegen::gen_client \
-    --with-watch \
-    --input-pkg-root k8s.io/sample-controller/pkg/apis \
-    --output-pkg-root k8s.io/sample-controller/pkg/generated \
-    --output-base "$(dirname "${BASH_SOURCE[0]}")/../../.." \
-    --boilerplate "${SCRIPT_ROOT}/hack/boilerplate.go.txt"
+echo $SCRIPT_ROOT
---
+++ pkg/apis/localstorage/v1/types.go
@@ -0,0 +1,38 @@
+package v1
+
+import (
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+)
+
+// +genclient
+// +k8s:deepcopy-gen:interfaces=k8s.io/apimachinery/pkg/runtime.Object
+
+// Foo is a specification for a Foo resource
+type Foo struct {
+	metav1.TypeMeta   `json:",inline"`
+	metav1.ObjectMeta `json:"metadata,omitempty"`
+
+	Spec   FooSpec   `json:"spec"`
+	Status FooStatus `json:"status"`
+}
+
+// FooSpec is the spec for a Foo resource
+type FooSpec struct {
+	DeploymentName string `json:"deploymentName"`
+	Replicas       *int32 `json:"replicas"`
+}
+
+// FooStatus is the status for a Foo resource
+type FooStatus struct {
+	AvailableReplicas int32 `json:"availableReplicas"`
+}
+
+// +k8s:deepcopy-gen:interfaces=k8s.io/apimachinery/pkg/runtime.Object
+
+// FooList is a list of Foo resources
+type FooList struct {
+	metav1.TypeMeta `json:",inline"`
+	metav1.ListMeta `json:"metadata"`
+
+	Items []Foo `json:"items"`
+}
