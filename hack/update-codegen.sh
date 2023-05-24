de3917ed20659322fdaa5a59f4cefc9755ab828c
caoyingjunz <cao.yingjunz@gmail.com>
2023-05-24 08:40:57 +0800

    add

--- hack/update-codegen.sh
+++ hack/update-codegen.sh
@@ -18,7 +18,19 @@ set -o errexit
 set -o nounset
 set -o pipefail
 
-./../code-generator/generate-groups.sh all \
-github.com/caoyingjunz/csi-driver-localstorage/pkg/client \
-github.com/caoyingjunz/csi-driver-localstorage/pkg/apis \
-localstorage.caoyingjunz.io:v1
+SCRIPT_ROOT=$(dirname "${BASH_SOURCE[0]}")/..
+CODEGEN_PKG=${CODEGEN_PKG:-$(cd "${SCRIPT_ROOT}"; ls -d -1 ./vendor/k8s.io/code-generator 2>/dev/null)}
+
+echo "${CODEGEN_PKG}"/generate-groups.sh "deepcopy,client,informer,lister" \
+       github.com/caoyingjunz/csi-driver-localstorage/pkg/client github.com/caoyingjunz/csi-driver-localstorage/pkg/apis localstorage.caoyingjunz.io:v1 \
+       --go-header-file "${SCRIPT_ROOT}"/hack/boilerplate.go.txt
+
+# generate the code with:
+# --output-base    because this script should also be able to run inside the vendor dir of
+#                  k8s.io/kubernetes. The output-base is needed for the generators to output into the vendor dir
+#                  instead of the $GOPATH directly. For normal projects this can be dropped.
+bash "${CODEGEN_PKG}"/generate-groups.sh "deepcopy,client,informer,lister" \
+  github.com/caoyingjunz/csi-driver-localstorage/pkg/client github.com/caoyingjunz/csi-driver-localstorage/pkg/apis localstorage.caoyingjunz.io:v1 \
+  --go-header-file "${SCRIPT_ROOT}"/hack/boilerplate.go.txt
+
+echo $CODEGEN_PKG
--- hack/verify-codegen.sh
+++ hack/verify-codegen.sh
@@ -18,12 +18,14 @@ set -o errexit
 set -o nounset
 set -o pipefail
 
-SCRIPT_ROOT="$(cd "$(dirname "${BASH_SOURCE[0]}")/.." && pwd -P)"
-DIFFROOT="${SCRIPT_ROOT}/pkg"
-TMP_DIFFROOT="$(mktemp -d -t "$(basename "$0").XXXXXX")/pkg"
+SCRIPT_ROOT=$(dirname "${BASH_SOURCE[0]}")/..
+
+DIFFROOT="${SCRIPT_ROOT}/examples"
+TMP_DIFFROOT="${SCRIPT_ROOT}/_tmp/examples"
+_tmp="${SCRIPT_ROOT}/_tmp"
 
 cleanup() {
-  rm -rf "${TMP_DIFFROOT}"
+  rm -rf "${_tmp}"
 }
 trap "cleanup" EXIT SIGINT
 
@@ -36,9 +38,20 @@ cp -a "${DIFFROOT}"/* "${TMP_DIFFROOT}"
 echo "diffing ${DIFFROOT} against freshly generated codegen"
 ret=0
 diff -Naupr "${DIFFROOT}" "${TMP_DIFFROOT}" || ret=$?
-if [[ $ret -eq 0 ]]; then
+cp -a "${TMP_DIFFROOT}"/* "${DIFFROOT}"
+if [[ $ret -eq 0 ]]
+then
   echo "${DIFFROOT} up to date."
 else
   echo "${DIFFROOT} is out of date. Please run hack/update-codegen.sh"
   exit 1
 fi
+
+# smoke test
+echo "Smoke testing examples by compiling..."
+pushd "./${SCRIPT_ROOT}/examples"
+  go build "k8s.io/code-generator/examples/crd/..."
+  go build "k8s.io/code-generator/examples/apiserver/..."
+  go build "k8s.io/code-generator/examples/MixedCase/..."
+  go build "k8s.io/code-generator/examples/HyphenGroup/..."
+popd
