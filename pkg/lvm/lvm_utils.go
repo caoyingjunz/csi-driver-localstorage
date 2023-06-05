cb144b93f09f2f0486e9f7a87964103d35aea67b
whou <whou@192.168.71.105>
2023-06-05 20:31:48 +0800

    update lvm unit and add unit test

--- pkg/lvm/lvm_utils.go
+++ pkg/lvm/lvm_utils.go
@@ -17,78 +17,55 @@ const (
 	lvRemove string = "lvremove"
 )
 
-type LogicalVolume struct {
-	Path   string
-	Name   string
-	VGName string
-	Size   string
-	ID     string
-}
-
-// 根据 CreateVolumeRequest 生成 LV struct
-func NewLogicalVolumeForCreate(pathPrefix string, id string, req *csi.CreateVolumeRequest) (*LogicalVolume, error) {
+func NewVolumeForCreate(pathPrefix string, id string, req *csi.CreateVolumeRequest) (*cache.Volume, string, error) {
 	name := req.GetName()
-	if len(name) == 0 {
-		return nil, errors.New("CreateVolumeRequest miss name")
-	}
-
-	if len(pathPrefix) == 0 {
-		return nil, errors.New("pathPrefix error")
-	}
-
 	// sc 中 spec.parameters 字段需要有 vg 的信息
 	vgname, ok := req.GetParameters()["vgname"]
 	if !ok {
 		klog.Info("create volume request sholud contain para of vgname")
-		return nil, errors.New("sc paras miss vgname")
+		return nil, "", errors.New("sc paras miss vgname")
+	}
+
+	if len(name) == 0 || len(pathPrefix) == 0 || len(id) == 0 || len(vgname) == 0 {
+		return nil, "", errors.New("name, path prefix, id or vgname error")
 	}
 
 	size := req.GetCapacityRange().GetRequiredBytes()
 	path := filepath.Join(pathPrefix, vgname, name)
 
-	return &LogicalVolume{
-		Path:   path,
-		Name:   name,
-		VGName: vgname,
-		Size:   fmt.Sprint(size),
-		ID:     id,
-	}, nil
+	return &cache.Volume{
+		VolName: name,
+		VolID:   id,
+		VolPath: path,
+		VolSize: size,
+	}, vgname, nil
 }
 
-// 根据 DeleteVolumeRequest 生成 LV struct
-func NewLogicalVolumeForDelete(cache cache.Cache, req *csi.DeleteVolumeRequest) (*LogicalVolume, error) {
+func NewVolumeForDelete(cache cache.Cache, req *csi.DeleteVolumeRequest) (*cache.Volume, error) {
 	volID := req.GetVolumeId()
+	if len(volID) == 0 {
+		return nil, errors.New("volume id error")
+	}
+
 	volume, err := cache.GetVolumeByID(volID)
 	if err != nil {
 		return nil, err
 	}
 
-	return &LogicalVolume{
-		Path: volume.VolPath,
-		Name: volume.VolName,
-		Size: fmt.Sprint(volume.VolSize),
-		ID:   volume.VolID,
-	}, nil
+	return &volume, nil
 }
 
 // lvcreate -n test -L 5Gi lvmvg
-func CreateLogicalVolume(lv *LogicalVolume) error {
+func CreateLogicalVolume(volume *cache.Volume, vgname string) error {
 	// 构造 lvcreate 的命令
 	var createLVArg []string
 
-	if len(lv.Name) == 0 || len(lv.VGName) == 0 {
-		klog.Info("lvname and vgname can't be empty")
-		return errors.New("miss lvname or vgname")
+	if len(vgname) == 0 {
+		return errors.New("vgname error")
 	}
 
-	// TODO: 优化 size 的校验方式
-	if len(lv.Size) == 0 {
-		klog.Info("lvsize can't be empty")
-		return errors.New("miss lvsize")
-	}
-
-	// lv 是否存在检查
-	exist, err := CheckVolumeExists(lv)
+	// volume 是否存在检查
+	exist, err := CheckVolumeExists(volume)
 	if err != nil {
 		return err
 	}
@@ -97,14 +74,14 @@ func CreateLogicalVolume(lv *LogicalVolume) error {
 		return errors.New("lv already exists")
 	}
 
-	createLVArg = append(createLVArg, "-n", lv.Name)
-	createLVArg = append(createLVArg, "-L", lv.Size)
-	createLVArg = append(createLVArg, lv.VGName)
+	createLVArg = append(createLVArg, "-n", volume.VolName)
+	createLVArg = append(createLVArg, "-L", fmt.Sprint(volume.VolSize))
+	createLVArg = append(createLVArg, vgname)
 
 	exec := exec.New()
 	out, err := exec.Command(lvCreate, createLVArg...).CombinedOutput()
 	if err != nil {
-		klog.Infof("lvcreate failed, lvname: %s, vgname: %s, size: %v\n", lv.Name, lv.VGName, lv.Size)
+		klog.Infof("lvcreate failed, lvname: %s, vgname: %s, size: %v\n", volume.VolName, vgname, volume.VolSize)
 		return errors.New("lvcreate failed")
 	}
 
@@ -112,8 +89,8 @@ func CreateLogicalVolume(lv *LogicalVolume) error {
 	return nil
 }
 
-func CheckVolumeExists(lv *LogicalVolume) (bool, error) {
-	if _, err := os.Stat(lv.Path); err != nil {
+func CheckVolumeExists(vol *cache.Volume) (bool, error) {
+	if _, err := os.Stat(vol.VolPath); err != nil {
 		if os.IsNotExist(err) {
 			return false, nil
 		}
@@ -123,19 +100,15 @@ func CheckVolumeExists(lv *LogicalVolume) (bool, error) {
 }
 
 // lvremove /dev/lvmvg/test -f
-func RemoveLogicalVolume(lv *LogicalVolume) error {
+func RemoveLogicalVolume(volume *cache.Volume) error {
 	// 构造 lvremove 的命令
 	var removeLVArg []string
 
 	// 删除前预检查
 	// TODO: 检查 lv 是否还是被 mount 的，可能存在误删除的情况，这里进行维护
-	if len(lv.Name) == 0 {
-		klog.Info("lvname can't be empty")
-		return errors.New("miss lvname")
-	}
 
 	// lv 是否存在检查
-	exist, err := CheckVolumeExists(lv)
+	exist, err := CheckVolumeExists(volume)
 	if err != nil {
 		return err
 	}
@@ -144,13 +117,13 @@ func RemoveLogicalVolume(lv *LogicalVolume) error {
 		return errors.New("lv doesn't exists")
 	}
 
-	removeLVArg = append(removeLVArg, lv.Path)
+	removeLVArg = append(removeLVArg, volume.VolPath)
 	removeLVArg = append(removeLVArg, "-f")
 
 	exec := exec.New()
 	out, err := exec.Command(lvRemove, removeLVArg...).CombinedOutput()
 	if err != nil {
-		klog.Infof("lvremove failed, lvname: %s, vgname: %s, size: %v\n", lv.Name, lv.VGName, lv.Size)
+		klog.Infof("lvremove failed, lvname: %s, size: %v\n", volume.VolName, volume.VolSize)
 		return errors.New("lvremove failed")
 	}
 
