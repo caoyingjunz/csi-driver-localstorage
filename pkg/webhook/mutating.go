c586c026605a2a607899bf43a41913f7b0ccac76
caoyingjunz <cao.yingjunz@gmail.com>
2023-06-11 18:02:11 +0800

    Validate and Set default value by API webhook (#167)

--- pkg/webhook/mutating.go
+++ pkg/webhook/mutating.go
@@ -21,6 +21,7 @@ import (
 	"encoding/json"
 	"net/http"
 
+	admissionv1 "k8s.io/api/admission/v1"
 	"k8s.io/klog/v2"
 	"sigs.k8s.io/controller-runtime/pkg/client"
 	"sigs.k8s.io/controller-runtime/pkg/webhook/admission"
@@ -37,6 +38,8 @@ type LocalstorageMutate struct {
 var _ admission.Handler = &LocalstorageMutate{}
 var _ admission.DecoderInjector = &LocalstorageMutate{}
 
+type SetFunc func(ls *localstoragev1.LocalStorage, op admissionv1.Operation)
+
 func (s *LocalstorageMutate) Handle(ctx context.Context, req admission.Request) admission.Response {
 	ls := &localstoragev1.LocalStorage{}
 	if err := s.decoder.Decode(req, ls); err != nil {
@@ -44,17 +47,13 @@ func (s *LocalstorageMutate) Handle(ctx context.Context, req admission.Request) 
 	}
 	klog.Infof("Mutating localstorage %s for %s", ls.Name, req.Operation)
 
-	// AddFinalizer
-	if !util.ContainsFinalizer(ls, util.LsProtectionFinalizer) {
-		util.AddFinalizer(ls, util.LsProtectionFinalizer)
-	}
-	// Set localstorage phase when created
-	if len(ls.Status.Phase) == 0 {
-		ls.Status.Phase = localstoragev1.LocalStoragePending
-	}
+	// add finalizer into localstorage if necessary
+	s.SetFinalizer(ls)
 
-	// TODO: set the other spec
+	// set localstorage default values
+	s.Default(ls, req.Operation, s.SetStatus, s.SetDisks, s.SetVolumes)
 
+	// PatchResponseFromRaw
 	data, err := json.Marshal(ls)
 	if err != nil {
 		return admission.Errored(http.StatusInternalServerError, err)
@@ -62,6 +61,56 @@ func (s *LocalstorageMutate) Handle(ctx context.Context, req admission.Request) 
 
 	klog.Infof("Mutated localstorage %+v for %s", ls, req.Operation)
 	return admission.PatchResponseFromRaw(req.Object.Raw, data)
+}
+
+// Default implements webhook.Defaulter so a webhook will be registered for the type
+func (s *LocalstorageMutate) Default(ls *localstoragev1.LocalStorage, op admissionv1.Operation, fn ...SetFunc) {
+	for _, f := range fn {
+		f(ls, op)
+	}
+}
+
+func (s *LocalstorageMutate) SetStatus(ls *localstoragev1.LocalStorage, op admissionv1.Operation) {
+	if op == admissionv1.Create {
+		if len(ls.Status.Phase) == 0 {
+			ls.Status.Phase = localstoragev1.LocalStoragePending
+		}
+	}
+}
+
+// SetDisks set the identifier to empty if provider when Created
+func (s *LocalstorageMutate) SetDisks(ls *localstoragev1.LocalStorage, op admissionv1.Operation) {
+	if op == admissionv1.Create {
+		disks := ls.Spec.Disks
+		if disks == nil {
+			return
+		}
+
+		var newDisk []localstoragev1.DiskSpec
+		for _, disk := range disks {
+			if len(disk.Name) == 0 {
+				continue
+			}
+			newDisk = append(newDisk, localstoragev1.DiskSpec{Name: disk.Name})
+		}
+
+		ls.Spec.Disks = newDisk
+	}
+}
+
+func (s *LocalstorageMutate) SetVolumes(ls *localstoragev1.LocalStorage, op admissionv1.Operation) {
+	if op == admissionv1.Create {
+		if len(ls.Status.Volumes) != 0 {
+			ls.Status.Volumes = nil
+		}
+	}
+}
+
+// SetFinalizer add finalizer if necessary
+func (s *LocalstorageMutate) SetFinalizer(ls *localstoragev1.LocalStorage) {
+	if !util.ContainsFinalizer(ls, util.LsProtectionFinalizer) {
+		util.AddFinalizer(ls, util.LsProtectionFinalizer)
+	}
 }
 
 // InjectDecoder implements admission.DecoderInjector interface.
--- pkg/webhook/validating.go
+++ pkg/webhook/validating.go
@@ -19,12 +19,14 @@ package webhook
 import (
 	"context"
 	"fmt"
-	v1 "k8s.io/api/core/v1"
+
 	"net/http"
 
+	v1 "k8s.io/api/core/v1"
 	"k8s.io/klog/v2"
 
 	admissionv1 "k8s.io/api/admission/v1"
+	validationutils "k8s.io/apimachinery/pkg/util/validation"
 	"sigs.k8s.io/controller-runtime/pkg/client"
 	"sigs.k8s.io/controller-runtime/pkg/webhook/admission"
 
@@ -47,11 +49,19 @@ func (v *LocalstorageValidator) Handle(ctx context.Context, req admission.Reques
 	klog.Infof("Validating localstorage %s for: %s", ls.Name, req.Operation)
 
 	var err error
+	if err = v.ValidateName(ctx, ls); err != nil {
+		admission.Errored(http.StatusBadRequest, err)
+	}
+
 	switch req.Operation {
 	case admissionv1.Create:
 		err = v.ValidateCreate(ctx, ls)
 	case admissionv1.Update:
-		err = v.ValidateUpdate(ctx, ls)
+		oldLocalstorage := &localstoragev1.LocalStorage{}
+		if err = v.decoder.DecodeRaw(req.OldObject, oldLocalstorage); err != nil {
+			return admission.Errored(http.StatusBadRequest, err)
+		}
+		err = v.ValidateUpdate(ctx, oldLocalstorage, ls)
 	case admissionv1.Delete:
 		err = v.ValidateDelete(ctx, ls)
 	}
@@ -62,18 +72,46 @@ func (v *LocalstorageValidator) Handle(ctx context.Context, req admission.Reques
 	return admission.Allowed("")
 }
 
+func (v *LocalstorageValidator) ValidateName(ctx context.Context, ls *localstoragev1.LocalStorage) error {
+	if len(ls.Name) == 0 {
+		return fmt.Errorf("localstorage (%s) name must be than 0 characters", ls.Name)
+	}
+	if len(ls.Name) > validationutils.DNS1035LabelMaxLength-11 {
+		return fmt.Errorf("localstorage (%s) name must be no more than 52 characters", ls.Name)
+	}
+
+	return nil
+}
+
 func (v *LocalstorageValidator) ValidateCreate(ctx context.Context, ls *localstoragev1.LocalStorage) error {
 	klog.V(2).Infof("validate create", "name", ls.Name)
 
 	if err := v.validateLocalStorageNode(ctx, ls); err != nil {
 		return err
 	}
+	if err := v.validateVolumeGroup(ctx, ls); err != nil {
+		return err
+	}
 
 	return nil
 }
 
-func (v *LocalstorageValidator) ValidateUpdate(ctx context.Context, ls *localstoragev1.LocalStorage) error {
-	klog.V(2).Infof("validate update", "name", ls.Name)
+func (v *LocalstorageValidator) ValidateUpdate(ctx context.Context, old, cur *localstoragev1.LocalStorage) error {
+	klog.V(2).Infof("validate update", "name", cur.Name)
+
+	// validate common object
+	if old.Name != cur.Name || old.APIVersion != cur.APIVersion || old.Kind != cur.Kind {
+		return fmt.Errorf("at least one of apiVersion, kind and name was changed")
+	}
+
+	// validate spec
+	if old.Spec.Node != cur.Spec.Node {
+		return fmt.Errorf("spec.node: Invalid value: %v: field is immutable", cur.Spec.Node)
+	}
+	if old.Spec.VolumeGroup != cur.Spec.VolumeGroup {
+		return fmt.Errorf("spec.volumeGroup: Invalid value: %v: field is immutable", cur.Spec.VolumeGroup)
+	}
+
 	return nil
 }
 
@@ -115,6 +153,14 @@ func (v *LocalstorageValidator) validateLocalStorageNode(ctx context.Context, ls
 		if localstorage.Spec.Node == ls.Spec.Node {
 			return fmt.Errorf("node (%s) already binded to the other localstorage", ls.Spec.Node)
 		}
+	}
+
+	return nil
+}
+
+func (v *LocalstorageValidator) validateVolumeGroup(ctx context.Context, ls *localstoragev1.LocalStorage) error {
+	if len(ls.Spec.VolumeGroup) == 0 {
+		return fmt.Errorf("spec.volumeGroup may not be empty")
 	}
 
 	return nil
