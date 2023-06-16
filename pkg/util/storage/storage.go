472d934d44303d72ca1edb8a78244d875c196d9b
caoyingjunz <cao.yingjunz@gmail.com>
2023-06-16 08:18:14 +0800

    add

--- pkg/util/storage/storage.go
+++ pkg/util/storage/storage.go
@@ -14,7 +14,7 @@ See the License for the specific language governing permissions and
 limitations under the License.
 */
 
-package localstorage
+package storage
 
 import (
 	localstoragev1 "github.com/caoyingjunz/csi-driver-localstorage/pkg/apis/localstorage/v1"
